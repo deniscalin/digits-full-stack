6c5fa48560e40b6d15a31d4ddff676d5105337e4
Denis Calin <deniscalin.pro@gmail.com>
2024-11-28 17:20:28 -0800

    Add /predict path to the server; add axios import to Canvas component

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+print('Loading app')
 
 # Enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
@@ -13,6 +15,13 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    response_obj = {'status': 'success'}
+    request_data = request.get_json()
+    print("Got this data from the front end: ", request_data)
+    response_obj['message'] = 'Received your request data!'
+    return jsonify(response_obj)
 
 if __name__ == '__main__':
     app.run()
